8b98cd3ce1cfb1afcbab52cb43f51b77ba049f38
jennywy <jennywy@gmail.com>
2017-09-27 10:10:53 -0400

    Add Name Attribute to Return via Handlebars

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,23 +2,24 @@
 const store = require('../store')
 
 const signUpSuccess = function (data) {
+  $('.content').trigger('reset')
   $('#message').text('Successfully signed up')
   $('#sign-up').hide()
 }
 
 const signUpFailure = function () {
+  $('.content').trigger('reset')
   $('#message').text('Error on sign up')
 }
 
 const signInSuccess = function (data) {
+  $('.content').trigger('reset')
   $('#message').text('Successfully signed in')
   store.user = data.user
   $('#sign-up').hide()
   $('#sign-in').hide()
   $('#sign-out').show()
   $('#change-password').show()
-  $('#get-game').show()
-  $('#get-games').show()
 }
 
 const signInFailure = function () {
@@ -26,6 +27,7 @@ const signInFailure = function () {
 }
 
 const signOutSuccess = function () {
+  $('.content').trigger('reset')
   $('#message').text('Signed Out Successfully')
   store.user = null
   $('#sign-in').show()
@@ -42,7 +44,7 @@ const signOutFailure = function () {
 
 const changePasswordSuccess = function () {
   $('#message').text('Changed password successfully')
-  $('#change-password').trigger('reset')
+  $('.content').trigger('reset')
 }
 
 const changePasswordFailure = function () {
