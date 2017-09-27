8c0da606b7d0a921d5331793a97b3093d57a8943
jennywy <jennywy@gmail.com>
2017-09-27 14:27:19 -0400

    Remove Extra Slash Production URL
    There was an extra slash that was preventing the production URL from
    making any requests

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://jwywdip2.herokuapp.com/',
+    production: 'https://jwywdip2.herokuapp.com',
     development: 'http://localhost:4741'
   }
 }
